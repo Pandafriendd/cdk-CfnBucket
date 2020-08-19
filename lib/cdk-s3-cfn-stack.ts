6c51f4180e4275898168478b36181d7f077a5230
Li <lizhiyua@38f9d35d438d.ant.amazon.com>
2020-08-18 23:10:17 -0400

    first commit

--- lib/cdk-s3-cfn-stack.ts
+++ lib/cdk-s3-cfn-stack.ts
@@ -1,18 +1,43 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as iam from "@aws-cdk/aws-iam";
+import * as s3 from "@aws-cdk/aws-s3";
 
 export class CdkS3CfnStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkS3CfnQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // RESOURCES
+
+    const replicaBucket = new s3.Bucket(this, "ReplicaBucket", {
+      versioned: true,
     });
 
-    const topic = new sns.Topic(this, 'CdkS3CfnTopic');
+    const role = new iam.Role(this, "ReplicaRole", {
+      assumedBy: new iam.ServicePrincipal("s3"),
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const realBucket = new s3.CfnBucket(this, "RealBucket", {
+      replicationConfiguration: {
+        role: role.roleArn,
+        rules: [
+          {
+            destination: {
+              bucket: replicaBucket.bucketArn
+            },
+            status: "Enabled",
+          },
+        ],
+      },
+      objectLockConfiguration: {
+        objectLockEnabled: 'Enabled',
+        rule: {
+          defaultRetention: {
+            days: 365
+          }
+        }
+      },
+      objectLockEnabled: true,
+    }
+    );
   }
 }
